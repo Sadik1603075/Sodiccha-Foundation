03f91e6012c7152153eb3f9a507da8487a71400e
Barik Sadik <bariksadik@gmail.com>
2020-10-30 02:22:08 +0600

    ADD: browser router.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Navigation from './components/Navigation/Navigataion'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App">     
+      <Router>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+        
+        <Route exact path='/'>
+        <Navigation />
+          index page
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigataion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navigataion() {
+    return (
+        <div style={{width: "100%", height: "40px", background: "#000"}}>
+            
+        </div>
+    )
+}
+
+export default Navigataion
